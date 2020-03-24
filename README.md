ec7479fe9e024b84a31c01f3240fba835935cc26
patrawalam <patrawalamurtaza@gmail.com>
2020-03-24 14:57:23 +0530

    Step 1 - Parsing the input list from user

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # testvagrantcodingchallenge
 Repository created for Coding Challenge provided by Test Vagrant
+
+1) Step 1 - Taking the input from the user in the form of comma seperated list. Splitting the list to String Array.
+Based on Paper selection, adding them to HashSet to ensure no duplicates are added
--- src/main/java/com/testvagrant/testingchallenge/Test.java
+++ src/main/java/com/testvagrant/testingchallenge/Test.java
@@ -1,10 +1,34 @@
 package com.testvagrant.testingchallenge;
 
+import java.util.HashSet;
+import java.util.Scanner;
+
 public class Test {
 
+	static Scanner scn = null;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		//The input to the program should be a comma separated list of papers that the consumer wants to subscribe.
+		
+		scn = new Scanner(System.in);
+		
+		System.out.println("Enter Papers whose monthly subscription needs to be calculated. Choices are :: TOI, Hindu, ET, BM and HT");
+		System.out.println("The input to the program should be a comma separated list of papers that the consumer wants to subscribe.");
+		System.out.print("Enter your string here :: ");
+		String str = scn.next();
+		str = str.replaceAll(" ", "");
+		HashSet<String> hs = new HashSet<String>();
+		
+		String[] strArr = str.split(",");
+		
+		for (String s : strArr) {
+			if(s.equalsIgnoreCase("TOI") || s.equalsIgnoreCase("Hindu") || s.equalsIgnoreCase("ET") 
+					|| s.equalsIgnoreCase("BM") || s.equalsIgnoreCase("HT"))
+				hs.add(s);
+		}
+		
+		System.out.println(hs);
 	}
 
 }

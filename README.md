5d919c501fc9760da53ac7f5fcc39a1d91a71669
patrawalam <patrawalamurtaza@gmail.com>
2020-03-24 15:49:32 +0530

    Added CategoryPrices test data file and Calculations Class to store
    Day/Value for each category using HashMap of HashMap

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # testvagrantcodingchallenge
 Repository created for Coding Challenge provided by Test Vagrant
 
-1) Step 1 - Taking the input from the user in the form of comma seperated list. Splitting the list to String Array.
+Step 1 - Taking the input from the user in the form of comma seperated list. Splitting the list to String Array.
 Based on Paper selection, adding them to HashSet to ensure no duplicates are added
+
+Step 2) Create an Excel called "CategoryPrices" which has a table created for category and prices based on day of the week.
+Created a Class called Calculations where the code to add Day/Value pair for each category using HashMap of HashMap
---
+++ src/main/java/com/testvagrant/testingchallenge/Calculations.java
@@ -0,0 +1,34 @@
+package com.testvagrant.testingchallenge;
+
+import java.util.HashMap;
+
+import com.testvagrant.testingchallenge.util.Xls_Reader;
+
+public class Calculations {
+
+	public static void main(String[] args) {
+
+		HashMap<String, HashMap<String, Double>> paper = new HashMap<String, HashMap<String, Double>>();
+		HashMap<String, Double> hm = null;
+		Xls_Reader xls = new Xls_Reader(
+				"D:\\SeleniumFiles\\EclipseWorkspaceForPractice\\TestingChallenge\\src\\test\\resources\\CategoryPrices.xlsx");
+
+		System.out.println(xls.getRowCount("Tables"));
+		
+		
+			for (int i = 2; i < xls.getRowCount("Tables"); i++) {
+				hm = new HashMap<String, Double>();
+				hm.put("Monday", Double.parseDouble(xls.getCellData("Tables", "Monday", i)));
+				hm.put("Tuesday", Double.parseDouble(xls.getCellData("Tables", "Tuesday", i)));
+				hm.put("Wednesday", Double.parseDouble(xls.getCellData("Tables", "Wednesday", i)));
+				hm.put("Thursday", Double.parseDouble(xls.getCellData("Tables", "Thursday", i)));
+				hm.put("Friday", Double.parseDouble(xls.getCellData("Tables", "Friday", i)));
+				hm.put("Saturday", Double.parseDouble(xls.getCellData("Tables", "Saturday", i)));
+				hm.put("Sunday", Double.parseDouble(xls.getCellData("Tables", "Sunday", i)));
+				
+				paper.put(xls.getCellData("Tables", "Category", i),hm);
+			}
+		
+			System.out.println(paper);
+	}
+}
